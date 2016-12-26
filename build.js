8a001223bb1dbdfb8601b22f0179cb923a18d387
Terrence Drumm <terrence.drumm@gmail.com>
2016-12-26 17:49:11 -0500

    fix local path error

--- build.js
+++ build.js
@@ -44,7 +44,7 @@ var
     author: 'Terrence Drumm',
     contact: 'https://twitter.com/lttdog',
     domain: devBuild ? 'http://127.0.0.1' : 'https://lttdog.github.io', // set domain
-    rootpath: devBuild ? null : '/EllenNethers/' // set absolute path (null for relative)
+    rootpath: devBuild ? '/' : '/EllenNethers/' // set absolute path (null for relative)
   },
 
   templateConfig = {
